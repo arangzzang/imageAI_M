d01098f2b1d9466a60702115f522b5c07dea4cad
Hyeon <dkfkd1198@gmail.com>
2023-06-06 00:41:08 +0900

    first System -H-

---
+++ src/main/java/com/freek/imageai_m/ImageAiMApplication.java
@@ -0,0 +1,13 @@
+package com.freek.imageai_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageAiMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageAiMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freek/imageai_m/ImageAiMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freek.imageai_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageAiMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
