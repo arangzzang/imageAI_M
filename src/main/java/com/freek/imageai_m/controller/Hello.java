d6ab81b181564ee3102628221fdea369f7a14988
Hyeon <dkfkd1198@gmail.com>
2023-06-10 08:37:50 +0900

    first System -H-

--- src/main/java/com/freek/imageai_m/controller/Hello.java
+++ src/main/java/com/freek/imageai_m/controller/Hello.java
@@ -1,13 +1,31 @@
 package com.freek.imageai_m.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@Slf4j
+@RequestMapping("/image")
+
 public class Hello {
 
-    @GetMapping("/test")
+    @GetMapping("/")
+    @ResponseBody
     public String test(){
-        return "test";
+        return "index";
+    }
+
+//    @RequestMapping(value="/", method = {RequestMethod.POST})
+    public Map<String,Object> multipartabc(MultipartHttpServletRequest req, MultipartFile file) {
+
+        Map<String,Object> result = new HashMap<String, Object>();
+
+        return result;
     }
 }
